e390c5776d7b5693bffaab76e9bd1fe6e5cfbb69
daryasv <svirskydarya@gmail.com>
2021-10-09 21:10:07 +0300

    fix

--- evolutionWebApp/src/chat/servlets/SendChatServlet.java
+++ evolutionWebApp/src/chat/servlets/SendChatServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "GetUserChatServlet", urlPatterns = {"/pages/chatroom/sendChat"})
+@WebServlet(name = "GetUserChatServlet", urlPatterns = {"/pages/lobby/sendChat"})
 public class SendChatServlet extends HttpServlet {
 
     /**
